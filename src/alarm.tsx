2e2b6fe001b6643f4fb8f4c2d8d4ed201d04863b
wandebergson <wandebergsonferreira@gmail.com>
2024-07-23 15:33:40 -0300

    date created

--- src/alarm.tsx
+++ src/alarm.tsx
@@ -2,11 +2,14 @@ import react from "react";
 import { useState } from "react";
 import "./alarm.css"
 import controlAlarm from "./controlAlarm";
+import nextAlarm from "./nextAlarm";
 
 function alarm(){
     return(
  <div className="alarm">
     <div className="logo"></div>
+    
+    {nextAlarm()}
 
     {controlAlarm()}
  </div>
